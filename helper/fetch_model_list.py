c33d5ab8ad6f847a3bdad8a9f645a3c409fb2a4a
avs-abhishek123 <abhishek.avs123@gmail.com>
2023-01-03 16:51:17 +0530

    fetch model list from API

---
+++ helper/fetch_model_list.py
@@ -0,0 +1,8 @@
+import os
+import openai
+import json
+
+openai.api_key = "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"
+json_dict = openai.Model.list()
+with open("openAI_models.json", "w") as outfile:
+    json.dump(json_dict, outfile, indent=4, sort_keys=True)
